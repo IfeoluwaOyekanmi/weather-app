d23136ef6736ea8b958bd459f3f2c0534033fe92
IfeoluwaOyekanmi <ifeoluwagodishere@gmail.com>
2024-05-30 22:06:34 +0100

    weather-card-completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import React, {useState}  from 'react';
+import axios from 'axios';  
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const url ='https://api.openweathermap.org/data/2.5/weather?lat=7.778825&lon=4.546455&appid=4d4323d78908e08ac921e0c09e0d2060'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Osogbo</p>
+          </div>
+          <div className="temp">
+            <h1>65°F</h1>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className='bold'>65°F</p>
+            <p >Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>20%</p>
+            <p>Humidity</p> 
+          </div>
+          <div className="wind">
+            <p className='bold'>12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
